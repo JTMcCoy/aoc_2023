6a53fa71acb9488c366ba7c5b526535f28b3b809
JTMcCoy <john.themba.mccoy@gmail.com>
2023-12-04 10:10:21 +0400

    add import of daily_run in main

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from data.input_data import input
+from src.runner import daily_run
 
-day = 1
+day = 4
 
-print(input[day])
+part1, part2 = daily_run(day)
+
+print(part1)
+print(part2)
+
+import re
+
+input = {}
+input[
+    day
+] = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
+# get each line as an entry
+values = [x for x in input[day].split("\n")]
