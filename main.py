75126dde2a41fb81d5fcd5feaf3a47ac29752883
JTMcCoy <john.themba.mccoy@gmail.com>
2023-12-08 10:09:29 +0400

    tidy main for day 9

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.runner import daily_run
 import time
 
-day = 8
+day = 9
 
 start = time.perf_counter_ns()
 part1, part2 = daily_run(day)
@@ -12,9 +12,7 @@ print(part2)
 print("Runtime: {:.3f}ms".format((end_t - start) / 1e6))
 
 input = {}
-input[
-    day
-] = """"""
+input[day] = """"""
 
 # get each line as an entry
 values = [x for x in input[day].split("\n")]
